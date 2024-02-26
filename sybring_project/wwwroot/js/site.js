69111924bec06ba3afffadb24de31e62a65d79d2
edan <gabriel_89_angel@hotmail.com>
2024-02-26 14:17:59 +0100

    layout fixing, moving javascript to where it should be, css fixing

--- sybring_project/wwwroot/js/site.js
+++ sybring_project/wwwroot/js/site.js
@@ -1,1 +1,13 @@
-﻿
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const themeLink = document.getElementById('theme-link');
+
+    themeToggleBtn.addEventListener('click', function () {
+        // Toggle between light and dark themes
+        if (themeLink.getAttribute('href') === '/css/light-theme.css') {
+            themeLink.setAttribute('href', '/css/dark-theme.css');
+        } else {
+            themeLink.setAttribute('href', '/css/light-theme.css');
+        }
+    });
+});
