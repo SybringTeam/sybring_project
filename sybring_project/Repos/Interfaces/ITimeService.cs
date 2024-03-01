11dd0171d8e2372828e25fca8e834e901f9eb7ba
spurti-salimath <spurti.salimath@gmail.com>
2024-03-01 13:24:53 +0100

    total working hours

--- sybring_project/Repos/Interfaces/ITimeService.cs
+++ sybring_project/Repos/Interfaces/ITimeService.cs
@@ -10,8 +10,9 @@ namespace sybring_project.Repos.Interfaces
         Task UpdateTimeHistoryAsync(TimeHistory updatedTimeHistory);
         Task DeleteTimeHistoryAsync(int id);
 
+        Task<Dictionary<string, double>> GenerateTimeReportByDaysAsync(DateTime startDate, DateTime endDate);
 
-       
+
 
     }
 }
