862720d2165c8c0640c4cac9dd55fd2477ae1efa
Dawood Rizwan <dawood.rizwan@outlook.com>
2024-03-08 09:58:36 +0100

    Project to user not completed

--- sybring_project/Repos/Services/ProjectServices.cs
+++ sybring_project/Repos/Services/ProjectServices.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.Build.ObjectModelRemoting;
+using Microsoft.EntityFrameworkCore;
 using sybring_project.Data;
 using sybring_project.Models.Db;
 using sybring_project.Repos.Interfaces;
@@ -21,6 +22,23 @@ namespace sybring_project.Repos.Services
 
         }
 
+        public async Task AssigUserToProjectAsync(string userId, int projectId)
+        {
+            var existingUser = await _db.Users
+                .Include(u => u.ProjectId)
+                .FirstOrDefaultAsync(u => u.Id == userId);
+
+            var projectToAdd = _db.Projects.FirstOrDefault(p => p.Id == projectId);
+
+
+            if (existingUser != null && projectToAdd != null)
+    {
+        existingUser.ProjectId.Add(projectToAdd);
+        await _db.SaveChangesAsync();
+    }
+           
+        }
+
         public async Task<Project> DeleteProjectAsync(int id)
         {
             var del = await _db.Projects.FindAsync(id);
@@ -33,8 +51,17 @@ namespace sybring_project.Repos.Services
 
         public async Task<Project> GetProjectByIdAsync(int id)
         {
-            return await _db.Projects.FindAsync(id);
+            var project = await _db.Projects.Include(p => p.Users)
+                                             .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (project == null)
+            {
+                throw new InvalidOperationException($"Project with ID {id} not found.");
+            }
+
+            return project;
         }
+
 
         public async Task<List<Project>> GetProjectsAsync()
         {
