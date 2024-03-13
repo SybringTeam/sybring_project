d5d98ca7e500d72c7fa4355fe707c1bfc77e7f44
spurti-salimath <spurti.salimath@gmail.com>
2024-03-13 11:25:04 +0100

    API

---
+++ sybring_project/Controllers/HoildayController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sybring_project.Repos.Interfaces;
+
+namespace sybring_project.Controllers
+{
+    public class HoildayController : Controller
+    {
+        private readonly IHoildayService _hoildayService;
+
+        public HoildayController(IHoildayService hoildayService)
+        {
+            _hoildayService = hoildayService;
+        }
+        //public IActionResult Index()
+        //{
+        //    return ViewComponent("HoildayViewComponent");
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            
+            var hoildayData = await _hoildayService.GetHoildayReport();
+
+            
+            return View(hoildayData);
+        }
+
+
+
+
+
+    }
+}
---
+++ sybring_project/Repos/Services/HoildayService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using sybring_project.Models;
+using sybring_project.Repos.Interfaces;
+using System.Net.Http;
+
+namespace sybring_project.Repos.Services
+{
+    public class HoildayService : IHoildayService
+    {
+        private readonly HttpClient _httpClient;
+        public HoildayService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClient = httpClientFactory.CreateClient("hoilday");
+        }
+
+
+        public async Task<Hoilday> GetHoildayReport()
+        {
+            var client = new HttpClient();
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"http://sholiday.faboul.se/dagar/v2.1/2024")
+            };
+            using var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<Hoilday>(content) ?? new Hoilday() { Datum = "No data available" };
+        }
+
+
+
+    }
+}
+
+
