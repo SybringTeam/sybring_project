fb79b6740f49fda023a8dab4fbaf858afb373736
spurti-salimath <spurti.salimath@gmail.com>
2024-04-18 10:29:43 +0200

    timereport

--- sybring_project/Controllers/TimeController.cs
+++ sybring_project/Controllers/TimeController.cs
@@ -75,7 +75,7 @@ namespace sybring_project.Controllers
         
         
         
-        //dowad work
+        
 
         //[Authorize(Roles = "Admin, underconsult")]
         [HttpGet]
@@ -113,7 +113,7 @@ namespace sybring_project.Controllers
 
 
 
-        //dowad work
+        
         //[Authorize(Roles = "Admin,underconsult")]
         [HttpPost]
         public async Task<IActionResult> Create(List<DayDataVM> weekData, decimal scheduledHoursPerWeek)
@@ -140,12 +140,10 @@ namespace sybring_project.Controllers
 
 
 
-        //fetch red days and weeknumbers of year from api
+        //fetching red days and weeknumbers of year from api
         public async Task<IActionResult> RedDays()
         {
 
-            //// Retrieve all red days for the current year
-            //var currentYear = DateTime.Now.Year;
 
             // Retrieve all red days for the year 2024
             var redDays = await _holidayService.GetRedDaysAsync();
