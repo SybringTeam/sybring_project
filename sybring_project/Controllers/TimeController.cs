b7953663d59b1b8506eedc0f9697921e529bf688
Edan <36916452+GabrielBunting@users.noreply.github.com>
2024-04-30 09:09:31 +0200

    fixing time detail

--- sybring_project/Controllers/TimeController.cs
+++ sybring_project/Controllers/TimeController.cs
@@ -238,7 +238,12 @@ namespace sybring_project.Controllers
 
 
 
-
+        [HttpGet]
+        public async Task<IActionResult> GetTimeHistoriesByWeek(string userId)
+        {
+            var timeHistories = await _timeService.GetHistoryByWeekNUser(userId, "week");
+            return Ok(timeHistories);
+        }
 
 
 
