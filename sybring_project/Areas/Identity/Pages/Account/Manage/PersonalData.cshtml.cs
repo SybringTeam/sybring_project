2447c737cff8a4940b622985ae95ec5c03b0f826
Natalieaktas <Natalieaktas@hotmail.com>
2024-05-03 13:04:38 +0200

    rätta vyn utan errors

--- sybring_project/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ sybring_project/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -1,21 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 using System;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.CodeAnalysis.Elfie.Extensions;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using sybring_project.Data;
 using sybring_project.Models.Db;
 using sybring_project.Repos.Interfaces;
-using System.Net.Http;
-using sybring_project.Models;
-
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace sybring_project.Areas.Identity.Pages.Account.Manage
 {
@@ -24,28 +16,18 @@ namespace sybring_project.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<User> _userManager;
         private readonly ILogger<PersonalDataModel> _logger;
         private readonly IUserServices _userServices;
-        private readonly ApplicationDbContext _db;
-        private readonly ICountryServices _countryServices;
 
-
-        public PersonalDataModel(UserManager<User> userManager,
-            ILogger<PersonalDataModel> logger, IUserServices userServices, ApplicationDbContext db,
-            ICountryServices countryServices)
+        public PersonalDataModel(
+            UserManager<User> userManager,
+            ILogger<PersonalDataModel> logger, IUserServices userServices)
         {
             _userManager = userManager;
             _logger = logger;
             _userServices = userServices;
-            _db = db;
-            _countryServices = countryServices;
-
         }
         [BindProperty]
+        public List<User> Users { get; set; }
         public User UserData { get; set; }
-        public List<Datum> ApiCountryNames { get; set; }
-
-
-
-
         public async Task<IActionResult> OnGet()
         {
             var user = await _userManager.GetUserAsync(User);
@@ -54,18 +36,13 @@ namespace sybring_project.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
 
-            // Fetch list of countries from API
-            var apiCountries = await _countryServices.GetAllCountriesAsync();
-            ApiCountryNames = new List<Datum>(apiCountries.data);
+            UserData = user;
 
             return Page();
         }
 
-
-
-
         public async Task<IActionResult> OnPost(string iceContactName, string iceContactNumber,
-            string selectedCountry, string address, string phone)
+            string citizenMembership)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
@@ -73,11 +50,11 @@ namespace sybring_project.Areas.Identity.Pages.Account.Manage
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserAsync(User)}'.");
             }
 
-            user.PhoneNumber = phone;
-            user.Address = address;
             user.ICEContactName = iceContactName;
             user.UserICE = iceContactNumber;
-            user.CitizenMembership = selectedCountry;
+            user.CitizenMembership = citizenMembership;
+            
+
 
 
             var result = await _userManager.UpdateAsync(user);
