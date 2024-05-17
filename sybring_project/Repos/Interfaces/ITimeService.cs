49c6109bfe5c1217f833e907889e648f4473f4e7
Natalieaktas <Natalieaktas@hotmail.com>
2024-05-17 13:26:01 +0200

    vyer

--- sybring_project/Repos/Interfaces/ITimeService.cs
+++ sybring_project/Repos/Interfaces/ITimeService.cs
@@ -40,12 +40,6 @@ namespace sybring_project.Repos.Interfaces
 
         void DeleteOldData();  //TimeTrigger method
 
-
-
-
-       
-
-
         Task ForgetTimeReport(Func<string, string, string, Task> sendEmailAsync); //TimeTrigger method
 
 
