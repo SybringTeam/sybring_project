7ab9ff53510e42224df0dec5c8bc06059f257537
spurti-salimath <spurti.salimath@gmail.com>
2024-05-17 10:25:45 +0200

    TimeReport View Details

--- sybring_project/Repos/Interfaces/ITimeService.cs
+++ sybring_project/Repos/Interfaces/ITimeService.cs
@@ -43,7 +43,7 @@ namespace sybring_project.Repos.Interfaces
 
 
 
-        void DeleteOldData();  //TimeTrigger method
+       
 
 
         Task ForgetTimeReport(Func<string, string, string, Task> sendEmailAsync); //TimeTrigger method
